43c052a50460beefb78be7d98b74017f49492af7
AlexKrauter <alexander.krauter@outlook.com>
2024-12-01 11:47:04 -0500

    Thing

--- Lesson-4/act1.py
+++ Lesson-4/act1.py
@@ -1,6 +1,6 @@
 import pygame
 icon = pygame.image.load("C:\\Users\\Cliff Krauter\\AIPython\\Lesson-4\\icon.png")
-
+X, O = pygame.image.load("C:\\Users\\Cliff Krauter\\AIPython\\Lesson-4\\X.png"), pygame.image.load("C:\\Users\\Cliff Krauter\\AIPython\\Lesson-4\\O.png")
 # pygame setup
 pygame.init()
 screen = pygame.display
@@ -9,13 +9,16 @@ screen.set_icon(icon)
 screen = screen.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+Tiles = [0,0,0,0,0,0,0,0,0]
 
-Tiles = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-]
+Wins = [["123"],["456"],["789"],["147"],["258"],["369"],["159"],["357"]]
 
+def draw_board():
+    list = []
+    for i in range(2):
+        list.append(pygame.draw.rect(screen, "white", pygame.Rect(ws[0]/4-20+(i*min(ws[0],ws[1]))/3,30,min(ws[0],ws[1])/3,(min(ws[0],ws[1])-60)/3)))
+    return list
+        
 while running:
     # check if player is inputting something
     for event in pygame.event.get():
@@ -33,10 +36,11 @@ while running:
     # render game
     screen.fill("skyblue")
     ws = pygame.display.get_window_size()
-    pygame.draw.rect(screen, "white", pygame.Rect(max(ws[0],ws[1])-min(ws[0],ws[1]),30,min(ws[0],ws[1]),min(ws[0],ws[1])-60))
+    draw_board()
+    
+    screen.blit(pygame.transform.scale(X,(min(ws[0],ws[1])/3,(min(ws[0],ws[1])-60)/3)), pygame.Rect(ws[0]/4-30,30,min(ws[0],ws[1]),min(ws[0],ws[1])-60))
     # flip() the display to put your work on screen
     pygame.display.flip()
-
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
